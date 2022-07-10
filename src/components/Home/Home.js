96e5546ec362a9beb408e30129e30232a28ca129
Xolbek-Developer <xolbekxalilov@gmail.com>
2022-07-10 17:38:01 +0500

    Portfolio section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,6 +18,37 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="skills">
+        <h1 className="skills-header">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </h1>
+        <div className="skills-container">
+          <div className="skill">
+            <img src="/assets/javascript.webp" />
+            <img src="/assets/typescript.webp" />
+            <img src="/assets/nodejs.webp" />
+          </div>
+          <div className="skill">
+            <img src="/assets/express.png" />
+            <img src="/assets/mongodb.png" />
+            <img src="/assets/postgresql.png" />
+          </div>
+          <div className="skill">
+            <img src="/assets/redis.png" />
+            <img src="/assets/docker.png" />
+            <img src="/assets/kubernetes.png" />
+          </div>
+          <div className="skill">
+            <img src="/assets/aws.webp" />
+            <img src="/assets/digitalocean.webp" />
+            <img src="/assets/react.png" />
+          </div>
+        </div>
+      </div>
+
+      <div className="portfolio"></div>
     </section>
   );
 };
